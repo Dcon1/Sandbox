4c640d5fba921994c9b8587eb3d5f828f277933d
danny <danny.connolly@my.jcu.edu.au>
2017-03-08 14:34:03 +1000

    Add odd name print program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,8 @@
 """
 Author: Danny Connolly
 """
+name = input("Please enter your name")
+while name == "":
+    name = input("Please enter your name")
+for i in range(0, len(name) + 1, 2):
+    print(name[i])
